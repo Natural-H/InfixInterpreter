b7961d851e1160ac4565aa5af397b357025e0d57
naturalh <marco.mauricio.244@outlook.com>
2023-09-08 20:17:43 -0600

    Adding the Stack class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(12);
+        stack.push(24);
+        stack.push(36);
+
+        System.out.println(stack);
     }
 }
---
+++ src/Stack.java
@@ -0,0 +1,78 @@
+public class Stack<T> {
+    private Object[] data;
+    private int used;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public Stack() {
+        data = new Object[DEFAULT_CAPACITY];
+        used = 0;
+    }
+
+    public Stack(int size) {
+        data = new Object[size];
+        used = 0;
+    }
+
+    public void push(T item) {
+        if (used == data.length) {
+            ensureCapacity(used * 2);
+        }
+        data[used++] = item;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            System.out.println("Couldn't remove element, stack is empty!");
+            return;
+        }
+        --used;
+        data[used] = null;
+    }
+
+    public T getTop() {
+        if (isEmpty()) {
+            System.out.println("There's no elements, stack is empty!");
+            return null;
+        }
+        @SuppressWarnings("unchecked")
+        T item = (T) data[used - 1];
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return used == 0;
+    }
+
+    public int getUsed() {
+        return used;
+    }
+
+    public void clear() {
+        for (int i = 0; i < used; i++) {
+            data[i] = null;
+        }
+        used = 0;
+    }
+
+    // Used to increase the stack capacity when it's necessary
+    private void ensureCapacity(int minCapacity) {
+        int newSize = Math.max(minCapacity, DEFAULT_CAPACITY);
+        Object[] newArray = new Object[newSize];
+        System.arraycopy(data, 0, newArray, 0, used);
+        data = newArray;
+    }
+
+    @Override
+    public String toString() {
+        // Can't use Arrays.toString(), it'll get the null data
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < used; i++) {
+            result.append(data[i]);
+            if (i < used - 1) {
+                result.append(", ");
+            }
+        }
+        result.append("]");
+        return result.toString();
+    }
+}
